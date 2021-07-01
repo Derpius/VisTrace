985eefe9ef720cc2d40b901e577201f6d8e43e00
100 Pixels Squared <49565664+100PXSquared@users.noreply.github.com>
2021-07-01 17:45:47 +0100

    Not sure why I would ever need separate addons

---
+++ package.bat
@@ -0,0 +1,1 @@
+"D:\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "Addon" -out "Release\Addon.gma"
--- publish_x64.bat
+++
@@ -1,1 +0,0 @@
-"D:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "Release\x64\Addon.gma" -id "2531198548" -changes %1
