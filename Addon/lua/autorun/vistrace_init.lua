96094b3b3d9011cb2c791003c936eb14ee07cb3a
Derpius <49565664+Derpius@users.noreply.github.com>
2021-12-20 22:09:51 +0000

    Fix Garry issue and gitignore gma scripts

--- Addon/lua/autorun/vistrace_init.lua
+++ Addon/lua/autorun/vistrace_init.lua
@@ -3,4 +3,19 @@ if SERVER then
 	return
 end
 
-pcall(require, "VisTrace-v0.4") -- Don't throw an error if the module failed to load
+--[[
+	https://github.com/Derpius/VisTrace/issues/13
+	pcall(require, "VisTrace-v0.4") -- Don't throw an error if the module failed to load
+]]
+
+if not file.Exists("lua/bin/gmcl_VisTrace-v0.4_win64.dll", "GAME") then return end
+
+if not system.IsWindows() then
+	error("VisTrace is currently only compatible with Windows, if you'd like a cross platform build to be worked on, please open an issue at https://github.com/Derpius/VisTrace/issues")
+end
+
+if jit.arch ~= "x64" then
+	error("VisTrace does not work on 32 bit builds of Garry's Mod at this time, please switch to the x86-64 branch in Steam betas")
+end
+
+require("VisTrace-v0.4")
